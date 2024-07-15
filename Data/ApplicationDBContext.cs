6bb78327b0829473e24b74d466d01e3c65dd9b5e
Vitor <vitorhugosnp@gmail.com>
2024-07-15 16:23:35 -0400

    Minor Update

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -2,10 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiTest.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiTest.Data;
 
-public class ApplicationDBContext
+public class ApplicationDBContext : DbContext
 {
-    
+    public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+
+    }
+    public DbSet<StockModel> Stock { get; set; }
+    public DbSet<CommentModel> Comments { get; set; }
 }
