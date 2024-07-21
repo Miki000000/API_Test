32119ae8fc3e09ba51e92089ba45e50487c3e68c
Vitor Hugo <vitorhugosnp@gmail.com>
2024-07-21 05:56:36 -0400

    Some Minor Changes

--- obj/Debug/net8.0/ApiTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+785e3b42fedc121a66c97474a01d0c7bf090a6a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e07041027aa7bb0d57a4e10d96ef6f27a171ea29")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
